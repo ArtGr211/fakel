e66c5cd693f2ba7edadbb55afa4d1d104a111be7
Art.Gr <artgr211@gmail.com>
2018-12-30 20:14:09 +0200

    Added pagination helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -43,7 +43,49 @@ const authorAccess = function (data, user, query, accessType) {
   return all ? all : own && data.author && data.author.equals(user._id);
 }
 
+const pagination = function (options) {
+  const pagination = {
+    current: options.current,
+    before: [],
+    after: []
+  }
+
+  const from = options.current - options.show;
+  const to = options.current + options.show + 1;
+
+  if (from > 1) {
+    pagination.first = {
+      index: 1,
+      link: options.link + 1
+    }
+  }
+
+  if (to < options.total) {
+    pagination.last = {
+      index: options.total,
+      link: options.link + (options.total)
+    }
+  }
+
+  for (let i = from >= 1 ? from : 1; i < options.current; i++) {
+    pagination.before.push({
+      index: i,
+      link: options.link + i
+    })
+  }
+
+  for (let i = options.current + 1; i < to && i <= options.total; i++) {
+    pagination.after.push({
+      index: i,
+      link: options.link + i
+    })
+  }
+
+  return pagination;
+}
+
 exports.removeEmpty = removeEmpty;
 exports.checkAccessByRole = checkAccessByRole;
 exports.checkBoxToBoolean = checkBoxToBoolean;
 exports.authorAccess = authorAccess;
+exports.pagination = pagination;
