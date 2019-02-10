8c55c3e3b81284de09738db3f63c059951d5a779
Art.Gr <artgr211@gmail.com>
2019-02-10 16:00:10 +0200

    Text editor

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,26 @@
 $(document).ready(function () {
+  // Quill
   $('.quill-editor').each(function () {
-    new Quill(this, {
+    const editorEl = $(this);
+    const textarea = editorEl.next('textarea');
+    const form = editorEl.closest('form');
+
+    const editor = new Quill(this, {
       modules: {
         toolbar: [
-          ['bold', 'italic', 'underline']
+          ['bold', 'italic', 'underline', 'link']
         ]
       }
     });
+
+    textarea.css({
+      display: 'none'
+    })
+
+    editor.on('text-change', function() {
+      textarea.val(
+        editorEl.find('.ql-editor').html()
+      );
+    })
   })
 })
