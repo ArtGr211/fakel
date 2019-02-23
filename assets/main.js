c4b0332f921d0a7500219540c6df21959874317f
Art.Gr <artgr211@gmail.com>
2019-02-23 14:22:18 +0200

    Removed video button from Quill editor

--- assets/main.js
+++ assets/main.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
             'link',
             'list',
             'image',
-            'video',
+            // 'video',
             'blockquote',
             'code-block'
           ]
